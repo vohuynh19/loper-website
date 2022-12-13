c6b67340e03f4e696af7b4d11e7bc45508ba8da6
vhuynh19 <vohuynh01092002@gmail.com>
2022-12-13 18:27:49 +0700

    WIP revamp

--- features/quest/ui/CreateQuest/styled.ts
+++ features/quest/ui/CreateQuest/styled.ts
@@ -1,4 +1,3 @@
-import { Form } from "antd";
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
@@ -7,14 +6,4 @@ export const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   min-height: 500px;
-`;
-
-export const Content = styled.div`
-  width: 600px;
-`;
-
-export const StyledForm = styled(Form)`
-  background-color: ${({ theme }) => theme.colors.hBg};
-  padding: ${({ theme }) => theme.spaces.xxl};
-  border-radius: ${({ theme }) => theme.spaces.xl};
 `;
--- features/quest/ui/QuestDashboard/styled.ts
+++ features/quest/ui/QuestDashboard/styled.ts
@@ -4,11 +4,3 @@ export const Wrapper = styled.div`
   display: flex;
   width: 100%;
 `;
-
-export const SideLeft = styled.div`
-  flex: 1;
-  padding: ${({ theme }) => `${theme.spaces.md} ${theme.spaces.lg}`};
-`;
-export const SideRight = styled.div`
-  width: 320px;
-`;
